e0e3c1b0c9cfc38ada27f704f2e413b88240472c
Spectre <antoine@doussaud.org>
2020-02-04 05:37:56 +0100

    update

--- main.lua
+++ main.lua
@@ -1,20 +1,48 @@
 local LEDsController = require "lib.LEDsController.LEDsController"
-require("lib.LoveFrames")
+local loveframes = require("lib.loveframes")
 
 
 local timer = 0
-local fps = 40
+local fps = 30
 local counter = 0
 
 local time = 0
 
-local lx = 40
-local ly = 20
+local lx = 64
+local ly = 8
 
 local json = require "lib.json"
 
 function rgb(r,g,b)
 	return r/255,g/255,b/255
+end
+
+
+function hslToRgb(h, s, l, a)
+	local r, g, b
+
+	if s == 0 then
+		r, g, b = l, l, l -- achromatic
+	else
+		function hue2rgb(p, q, t)
+			if t < 0   then t = t + 1 end
+			if t > 1   then t = t - 1 end
+			if t < 1/6 then return p + (q - p) * 6 * t end
+			if t < 1/2 then return q end
+			if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+			return p
+		end
+
+		local q
+		if l < 0.5 then q = l * (1 + s) else q = l + s - l * s end
+		local p = 2 * l - q
+
+		r = hue2rgb(p, q, h + 1/3)
+		g = hue2rgb(p, q, h)
+		b = hue2rgb(p, q, h - 1/3)
+	end
+
+	return r, g, b, a
 end
 
 
@@ -27,8 +55,8 @@ function love.load(arg)
 
 	poke = love.graphics.newImage("ressource/antoine.png")
 
-	controller = LEDsController:new(lx*ly, "artnet", "192.168.1.210")--"10.80.1.18")
-	controller:loadMap(json.decode(love.filesystem.read("map/map_20x20.json")))
+	controller = LEDsController:new(lx*ly, "RGB888", "192.168.1.210")--"10.80.1.18")
+	controller:loadMap(json.decode(love.filesystem.read("map/map_hat_bis.json")))
 	controller.rgbw = false
 	controller.leds_by_uni = 170
 
@@ -49,18 +77,143 @@ function love.load(arg)
 		shaders[k].name = v
 	end
 
+	for k,v in pairs(loveframes.skins) do print(k,v) end
+
+	-- loveframes.SetActiveSkin("Orange")
+	loveframes.SetActiveSkin("Default red")
+
+	local frame = loveframes.Create("frame")
+	frame:SetName("Animation")
+	frame:SetSize(40*10, 20*10)
+	frame:SetResizable(true)
+	frame:SetMaxWidth(800)
+	frame:SetMaxHeight(600)
+	frame:SetMinWidth(200)
+	frame:SetMinHeight(200)
+
+	local panel = loveframes.Create("panel", frame)
+	panel:SetPos(4, 28)
+	panel:SetSize(frame:GetWidth(), frame:GetHeight())
+
+	panel.Draw = function(object)
+		love.graphics.setColor(1, 1, 1)
+		love.graphics.draw(
+			canvas,
+			object:GetX(),
+			object:GetY(),
+			0,
+			(object:GetWidth()-4)/lx,
+			(object:GetHeight()-4)/ly
+		)
+	end
+
+	panel.Update = function(object)
+		object:SetSize(frame:GetWidth()-2, frame:GetHeight()-2-26)
+	end
+
+	local frame2 = loveframes.Create("frame")
+	frame2:SetName("Network Discovery")
+	frame2:SetSize(600, 400)
+	frame2:SetPos(0, 300)
+	-- frame2:SetResizable(true)
+
+	local button = loveframes.Create("button", frame2)
+	button:SetWidth(200)
+	button:SetPos(5, 30)
+	button:SetText("Scan network")
+	-- button:Center()
+	button.OnClick = function(object, x, y)
+		node_list:Clear()
+		controller:sendArtnetPoll()
+	end
+
+	node_list = loveframes.Create("columnlist", frame2)
+	node_list:SetPos(5, 60)
+	node_list:SetSize(frame2:GetWidth()-10, frame2:GetHeight()-60-5)
+	node_list:AddColumn("Name")
+	node_list:AddColumn("ip")
+	node_list:AddColumn("port")
+	node_list:AddColumn("net")
+	node_list:AddColumn("subnet")
+	node_list:AddColumn("status")
+
+	local frame3 = loveframes.Create("frame")
+	frame3:SetName("Pixel Map")
+	-- frame3:SetSize(300, 715)
+	frame3:SetSize(890, 715)
+	frame3:SetPos(0, 300)
+
+	local grid = loveframes.Create("grid", frame3)
+	grid:SetPos(5, 30)
+	grid:SetRows(#controller.map[1])
+	grid:SetColumns(#controller.map)
+	grid:SetCellWidth(40)
+	grid:SetCellHeight(30)
+	grid:SetCellPadding(2)
+	grid:SetItemAutoSize(true)
+
+	local id = 1
+
+	for x=1, #controller.map do
+		for y=1, #controller.map[1] do
+			local m = controller.map[x][y]
+			local ur,ug,ub = hslToRgb(m.uni/5,1,0.4)
+			local ir,ig,ib = hslToRgb(m.id/100,1,0.0)
+			local text = {
+				{color = {ur, ug, ub}},
+				"Uni:"..m.uni,
+				{color = {ir, ig, ib}},
+				"\nID:"..m.id,
+			}
+			local text1 = loveframes.Create("text")
+			text1:SetText(text)
+			grid:AddItem(text1, y, x)
+		end
+	end
+
+	local frame3 = loveframes.Create("frame")
+	frame3:SetName("Network Map")
+	frame3:SetSize(600, 250)
+	frame3:SetPos(0, 600)
+
+	network_map = loveframes.Create("columnlist", frame3)
+	network_map:SetPos(5, 30)
+	network_map:SetSize(frame3:GetWidth()-10, frame3:GetHeight()-30-5)
+	network_map:AddColumn("net")
+	network_map:AddColumn("subnet")
+	network_map:AddColumn("ip")
+	network_map:AddColumn("port")
+	network_map:AddColumn("Sync")
+	network_map:AddColumn("On")
+
+	for i=0,8 do
+		print(info)
+		network_map:AddRow(
+			0,
+			i,
+			"192.168.1."..i,
+			6454,
+			"False",
+			"True"
+		)
+	end
+
+	local image = love.graphics.newImage("ressource/bg.png")
+	image:setWrap("repeat", "repeat")
+	local width = love.graphics.getWidth()
+	local height = love.graphics.getHeight()
+	bgquad = love.graphics.newQuad(0, 0, width, height, image:getWidth(), image:getHeight())
+	bgimage = image
 end
 
 function love.joystickpressed( joystick, button )
-	print( joystick, button)
+	print(joystick, button)
 
 end
 
 function love.draw()
-	-- love.graphics.setShader(shaders[shader_nb].shader)
-	love.graphics.setColor(1, 1, 1, 1)
-	love.graphics.draw(canvas,0,0,0,12,12);
-	-- love.graphics.setShader()
+	love.graphics.draw(bgimage, bgquad, 0, 0)
+	loveframes.draw()
 end
 
 function love.update(dt)
@@ -88,7 +241,7 @@ function love.update(dt)
 				controller:setArtnetLED(x, y, {r*255, g*255, b*255})
 			end
 		end
-		controller:send(1/fps, true)
+		-- controller:send(1/fps, true)
 	end
 
 	if shaders[shader_nb] then
@@ -104,9 +257,50 @@ function love.update(dt)
 		end
 	end
 
+	local data, ip, port = controller.udp:receivefrom()
+	if data then
+		local type, info = controller:receiveArtnet(data, ip, port)
+		if type == "reply" then
+			-- for i=0,20 do
+				print(info)
+				node_list:AddRow(
+					info.short_name,
+					info.ip[1].."."..info.ip[2].."."..info.ip[3].."."..info.ip[4],
+					info.port,
+					info.net,
+					info.subnet,
+					info.status
+				)
+			-- end
+		end
+	end
+
+	loveframes.update(dt)
+end
+
+function love.mousepressed(x, y, button)
+	loveframes.mousepressed(x, y, button)
+end
+
+
+function love.mousereleased(x, y, button)
+	loveframes.mousereleased(x, y, button)
 end
 
 function love.keypressed( key, scancode, isrepeat )
 	if key == "up" and shader_nb > 1 then shader_nb = shader_nb - 1 end
 	if key == "down" and shader_nb < #shaders then shader_nb = shader_nb + 1 end
+	loveframes.keypressed(key, unicode)
+end
+
+function love.keyreleased(key)
+	loveframes.keyreleased(key)
+end
+
+function love.wheelmoved(x, y)
+	loveframes.wheelmoved(x, y)
+end
+
+function love.resize(w, h)
+	bgquad = love.graphics.newQuad(0, 0, w, h, bgimage:getWidth(), bgimage:getHeight())
 end
