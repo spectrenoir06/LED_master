67dd96b8ae3568d3e2be112550302b7137a6c3cb
Spectre <antoine.doussaud.org>
2020-02-10 20:35:22 +0100

    opti map

--- thread_led_controller.lua
+++ thread_led_controller.lua
@@ -11,16 +11,15 @@ controller = LEDsController:new(t)
 while true do
 	local data = love.thread.getChannel('img'):pop()
 	if data then
-		local leds = controller.leds
-		for x=0,data:getWidth()-1,1 do
-			for y=0,data:getHeight()-1,1 do
-				local r,g,b = data:getPixel(x, y)
-				-- local w = (math.max(r,g,b) + math.min(r,g,b)) / 2
-				local w = math.min(r,g,b)
-				-- r,g,b = r-w, g-w, b-w
-				-- local w = 0
-				controller:setArtnetLED(x, y, {r*255, g*255, b*255, w*255})
-			end
+		for k,v in ipairs(controller.map) do
+			local r,g,b = data:getPixel(v.x, v.y)
+
+			-- local w = (math.max(r,g,b) + math.min(r,g,b)) / 2
+			local w = math.min(r,g,b)
+			r,g,b = r-w, g-w, b-w
+			-- local w = 0
+
+			controller:setArtnetLED(v, {r*255, g*255, b*255, w*255})
 		end
 		controller:send(1/fps/2, sync)
 	end
@@ -35,16 +34,14 @@ while true do
 		-- print(ip,port)
 		local type, info = controller:receiveArtnet(data, ip, port)
 		if type == "reply" then
-			-- for i=0,20 do
-				controller:addArtnetNode(
-					info.net,
-					info.subnet,
-					info.ip[1].."."..info.ip[2].."."..info.ip[3].."."..info.ip[4],
-					info.port,
-					nb
-				)
-				love.thread.getChannel('node'):push(info)
-			-- end
+			controller:addArtnetNode(
+				info.net,
+				info.subnet,
+				info.ip[1].."."..info.ip[2].."."..info.ip[3].."."..info.ip[4],
+				info.port,
+				nb
+			)
+			love.thread.getChannel('node'):push(info)
 		end
 	end
 end
