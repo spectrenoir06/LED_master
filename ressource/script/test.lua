18c0c2c7aa7e6093ea6d016f784cd81a939e9987
Spectre <antoine@doussaud.org>
2020-03-01 15:48:59 +0100

    test

--- ressource/script/test.lua
+++ ressource/script/test.lua
@@ -7,14 +7,14 @@ font:setFilter("nearest","nearest")
 function test:update(dt, lx, ly)
 	love.graphics.setFont(font)
 	love.graphics.clear(0,0,0,1)
-	for x=0,6 do
-		for y=0,1 do
-			local r,g,b = hslToRgb((x+y*4)/8,1,0.2)
+	for x=0,math.floor(lx/10)-1 do
+		for y=0,math.floor(ly/10)-1 do
+			local r,g,b = hslToRgb((x+y*(math.floor(lx/10)))/11,1,0.2)
 			love.graphics.setColor(r,g,b)
 			love.graphics.rectangle("fill", x*10, y*10, 10, 10)
 
 			love.graphics.setColor(1,1,1)
-			love.graphics.print(x+y*4, x*10+1, y*10)
+			love.graphics.print((x+y*(math.floor(lx/10)))%10, x*10+1, y*10)
 		end
 	end
 end
