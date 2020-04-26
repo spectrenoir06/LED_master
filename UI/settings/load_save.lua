3930a47f1b63351da8d5109c3c1b7a0adc4fe332
Spectre <antoine@doussaud.org>
2020-04-26 14:20:34 +0200

    list load

--- UI/settings/load_save.lua
+++ UI/settings/load_save.lua
@@ -3,38 +3,41 @@ local load_save = {}
 function load_save:load(loveframes, frame, tabs, start_y, step_y)
 
 	self.panel_load_save = loveframes.Create("panel")
-	tabs:AddTab("Load/Save", self.panel_load_save, nil, "ressource/icons/json.png")
+	tabs:AddTab("Load/Save", self.panel_load_save, nil, "ressource/icons/json.png", function() self:reload() end)
 
-	self.choice_file = loveframes.Create("multichoice", self.panel_load_save)
-	self.choice_file:SetPos(8, 8)
-	self.choice_file:SetSize(self.panel_load_save:GetWidth()-16, 25)
+	-- self.choice_file = loveframes.Create("multichoice", self.panel_load_save)
+	-- self.choice_file:SetPos(8, 8)
+	-- self.choice_file:SetSize(self.panel_load_save:GetWidth()-16, 25)
 
-	local list = loveframes.Create("columnlist", self.panel_load_save)
-	list:SetPos(8, 38)
-	list:SetSize(self.panel_load_save:GetWidth()-16, self.panel_load_save:GetHeight()-38-8)
-	list:AddColumn("Name").children[1].width = 120
-	list:AddColumn("lx").children[2].width = 30
-	list:AddColumn("ly").children[3].width = 30
-	list:AddColumn("fps").children[4].width = 30
-	list:AddColumn("nodes").children[4].width = 30
-	list:AddColumn("pixels").children[5].width = 30
+	self.list = loveframes.Create("columnlist", self.panel_load_save)
+	self.list:SetPos(8, 8)
+	self.list:SetSize((self.panel_load_save:GetWidth()-16)/2, self.panel_load_save:GetHeight()-16)
+	self.list:AddColumn("Name").children[1].width = 120
+	self.list:AddColumn("lx").children[2].width = 30
+	self.list:AddColumn("ly").children[3].width = 30
+	self.list:AddColumn("fps").children[4].width = 30
+	self.list:AddColumn("nodes").children[5].width = 40
+	self.list:AddColumn("pixels").children[6].width = 50
+	self.list:AddColumn("change").children[7].width = 40
+	--
+	self.list:SetSelectionEnabled(true)
+	-- self.list:SelectRow(1)
 
-	for k,v in pairs(maps) do
-		self.choice_file:AddChoice(v.name)
-		list:AddRow(v.name, v.lx, v.ly, v.fps, #v.nodes, #v.map)
-	end
-	self.choice_file:SelectChoice("42.map")
+	self:reload()
+
+	-- self.choice_file:SelectChoice("42.map")
 
 	self.panel_load_save:SetSize(frame:GetWidth()-16, frame:GetHeight()-60-4)
 
 	self.panel_load_save.Update = function(object)
 		object:SetSize(frame:GetWidth()-16, frame:GetHeight()-60-4)
-		self.choice_file:SetWidth(object:GetWidth()-16)
-		list:SetSize(object:GetWidth()-16, object:GetHeight()-38-8)
+		-- self.choice_file:SetWidth(object:GetWidth()-16)
+		self.list:SetSize((object:GetWidth()-16)/2, object:GetHeight()-16)
 	end
 
-	self.choice_file.OnChoiceSelected = function(object, choice)
-		mapping = maps[choice]
+	self.list.OnRowSelected = function(object, row, data)
+		print("OnRowSelected", data[1])
+		mapping = maps[data[1]]
 		local channel_data = love.thread.getChannel("data")
 		channel_data:push({type = "map", data = mapping.map})
 		channel_data:push({type = "nodes", data = mapping.nodes})
@@ -43,6 +46,21 @@ function load_save:load(loveframes, frame, tabs, start_y, step_y)
 		canvas_test = love.graphics.newCanvas(mapping.lx, mapping.ly, {dpiscale = 1, mipmaps = "none"})
 		canvas:setFilter("nearest", "nearest")
 		canvas_test:setFilter("nearest", "nearest")
+	end
+
+	for k,v in pairs(maps) do
+		self.list:AddRow(v.name, v.lx, v.ly, v.fps, #v.nodes, #v.map, diff)
+	end
+
+	self.list:SelectRow(self.list.internals[1].children[1])
+
+end
+
+function load_save:reload()
+	for k,v in ipairs(self.list.internals[1].children) do
+		local name = v.columndata[1]
+		local diff = (gen_map_file(maps[name]) ~= love.filesystem.read("ressource/map/"..name))
+		v.columndata[7] = tostring(diff)
 	end
 end
 
