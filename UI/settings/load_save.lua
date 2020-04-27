5b92ee5f142d48c83694b95998e8437d7542f203
Spectre <antoine@doussaud.org>
2020-04-27 15:45:46 +0200

    add undo

--- UI/settings/load_save.lua
+++ UI/settings/load_save.lua
@@ -1,3 +1,5 @@
+local json = require("lib.json")
+
 local load_save = {}
 
 function copy(obj, seen)
@@ -79,16 +81,16 @@ function load_save:load(loveframes, frame, tabs, start_y, step_y)
 				nb = nb + 1
 			end
 		end
+
 		maps[new_name] = copy(maps[self.row_selected[1]])
 		maps[new_name].name = new_name
 
-		local data = gen_map_file(maps[new_name])
-		love.filesystem.write("ressource/map/"..new_name, data)
+		-- local data = gen_map_file(maps[new_name])
+		-- love.filesystem.write("ressource/map/"..new_name, data)
 
 		self.list:AddRow(maps[new_name].name, maps[new_name].lx, maps[new_name].ly, maps[new_name].fps, #maps[new_name].nodes, #maps[new_name].map, false)
 		self.list:SelectRow(self.list.internals[1].children[#self.list.internals[1].children])
 
-		-- self:load_list()
 		self:reload()
 	end
 
@@ -108,17 +110,24 @@ function load_save:load(loveframes, frame, tabs, start_y, step_y)
 		self:reload()
 	end
 
+	self.button_undo = loveframes.Create("button", self.panel_load_setting)
+	self.button_undo:SetWidth(60)
+	self.button_undo:SetText("Undo")
+	self.button_undo:SetImage("ressource/icons/clock-history-frame.png")
+	self.button_undo:SetPos(8, step_y*2+8)
+
+	self.button_undo.OnClick = function(object, x, y)
+		local name = self.row_selected[1]
+		print("undo", name)
+		maps[name] = json.decode(love.filesystem.read("ressource/map/"..name))
+		self.row_selected[7] = "false"
+	end
+
 
 	self.textinput_name = loveframes.Create("textinput", self.panel_load_setting)
 	self.textinput_name:SetPos(8, step_y*0+8)
 	self.textinput_name:SetWidth(230-16-8)
 	-- self.textinput_name:SetFont(love.graphics.newFont(12))
-
-	local menu = loveframes.Create("menu")
-	menu:AddOption("Duplicate", "ressource/icons/node-design.png", nil)
-	menu:AddOption("Remove map", "ressource/icons/disk--minus.png", nil)
-	menu:AddOption("Remove change", "ressource/icons/disk--minus.png", nil)
-	menu:SetVisible(false)
 
 	self.row_selected = nil
 
@@ -135,18 +144,12 @@ function load_save:load(loveframes, frame, tabs, start_y, step_y)
 		canvas_test:setFilter("nearest", "nearest")
 
 		self.button_save:SetEnabled(data[7] == "true")
+		self.button_undo:SetEnabled(data[7] == "true")
 		self.row_selected = data
 		self.textinput_name:SetText(data[1]:match("^(.+).map$"))
 	end
 
-	self.list.OnRowRightClicked = function(parent, row, data)
-		menu:SetPos(love.mouse.getX(), love.mouse.getY())
-		menu:SetVisible(true)
-		menu:MoveToTop()
-	end
-
 	self:load_list()
-	menu:SetVisible(false)
 
 end
 
@@ -166,6 +169,7 @@ function load_save:reload()
 		v.columndata[7] = tostring(diff)
 	end
 	self.button_save:SetEnabled(self.row_selected[7] == "true")
+	self.button_undo:SetEnabled(self.row_selected[7] == "true")
 end
 
 return load_save
